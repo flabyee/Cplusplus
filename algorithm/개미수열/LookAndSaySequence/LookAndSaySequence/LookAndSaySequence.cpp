a56e473b43ce609f62175b04863d271ceb3abe8c
DESKTOP-77DPHAE\ROG <agape513513@naver.com>
2021-05-27 18:43:22 +0900

    개미수열

---
+++ algorithm/개미수열/LookAndSaySequence/LookAndSaySequence/LookAndSaySequence.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+#pragma warning(disable : 4996)
+
+using namespace std;
+
+int main()
+{
+	string s = "1";
+
+	for (int i = 0; i < 10; i++)
+	{
+		string end ="";
+		char num = s[0];
+		int count = 0;
+
+		for (int j = 0; j < s.length(); j++)
+		{
+			if(num  == s[j])
+			{
+				count++;
+			}
+			else
+			{
+				string temp = "";
+				temp += num;
+				end += (temp + to_string(count));
+				count = 1;
+				num = s[j];
+			}
+		}
+		string temp = "";
+		temp += num;
+		end += (temp + to_string(count));
+		s = end;
+		cout << s << endl;
+	}
+}
+
